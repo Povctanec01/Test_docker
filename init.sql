bb928b69995c41daa7d5babd8667aa9fc3dbd6ca
Povstanec <maks-katalnikov@mail.ru>
2026-02-06 20:36:29 +0300

    init

--- init.sql
+++ init.sql
@@ -1,2 +1,11 @@
+-- Создаем пользователя и БД
+CREATE USER myuser WITH PASSWORD 'mypassword';
+CREATE DATABASE django_db OWNER myuser;
+
+-- Права для PostgreSQL 15+
 GRANT ALL ON SCHEMA public TO myuser;
 ALTER SCHEMA public OWNER TO myuser;
+
+-- Тестовая таблица
+\c django_db
+CREATE TABLE IF NOT EXISTS test (id SERIAL PRIMARY KEY, created_at TIMESTAMP DEFAULT NOW());
